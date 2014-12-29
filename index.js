10459f16c554b7244932f645966aecb73a791644
1egoman <rsg1egoman@gmail.com>
2014-12-28 21:22:28 -0500

    refactored into module form

--- index.js
+++ index.js
@@ -7,204 +7,227 @@ var host = "http://127.0.0.1:8000";
 
 // the query
 
-// stem a whole array
-var stemArray = function(array) {
-  stems = [];
-  array.forEach(function(i) {
-    stems.push( natural.PorterStemmer.stem(i) );
-  });
+module.exports = Parser = function() {
+  var root = this;
 
-  return stems;
-}
+  // all possible types
+  this.phraseTypes = {
+    equality: [/(.*) equals?(?: to)? (.*)/gi],
+    enable: [/(.*) on (?:the )?(.*)/gi, /enable/gi],
+    disable: [/(.*) off (?:the )?(.*)/gi, /enable/gi],
+    status_default_false: [/is (?:.*) ?off/gi, /is (?:.*) ?false/gi],
+    status_default_true: [/is (?:.*) ?on/gi, /is (?:.*) ?true/gi],
+    status: [/is/gi, /status/gi]
+  };
 
+  // break down the phrase into a more descriptive, structured form
+  this.breakDownPhrase = function(raw, callback) {
+    action(raw, {}, function(data) {
 
-// finds the record that matches the best, out of all the choices
-var getCorrectRecord = function(raw, callback) {
-  action(raw, {}, function(data) {
+      // sample services list
+      // plugins = [
+      //   {
+      //     name: "gmail",
+      //     desc: "Check email remotely",
+      //     id: 0,
+      //     tags: ["email", "gmail", "mail", "webmail"],
+      //     data: {}
+      //   }
+      // ]
 
-    // sample services list
-    // plugins = [
-    //   {
-    //     name: "gmail",
-    //     desc: "Check email remotely",
-    //     id: 0,
-    //     tags: ["email", "gmail", "mail", "webmail"],
-    //     data: {}
-    //   }
-    // ]
+      // console.log(data)
 
-    // console.log(data)
-
-    // function() {
-      // var plugins = JSON.parse(body).data;
-      var plugins = [
-        {
-          name: "basementled",
-          desc: "sample plugin",
-          location: "basement",
-          data: {
-            led: {
-              value: false
+      // function() {
+        // var plugins = JSON.parse(body).data;
+        var plugins = [
+          {
+            name: "basementled",
+            desc: "sample plugin",
+            location: "basement",
+            data: {
+              led: {
+                value: false
+              },
+              lamp: {
+                value: false
+              }
+            }
+          },
+          {
+            name: "kitchenled",
+            desc: "sample plugin",
+            location: "kitchen",
+            data: {
+              led: {
+                value: false
+              }
             }
           }
-        },
-        {
-          name: "kitchenled",
-          desc: "sample plugin",
-          location: "kitchen",
-          data: {
-            led: {
-              value: false
+        ];
+
+        // try to match the query to a plugin
+        var count = [];
+        _.each(plugins, function(p) {
+          inputTotal = 0;
+          _.each(data.words, function(word) {
+
+            // find the common synonyms/tags between the plugin and the word
+            // all parts of the plugin (that matter)
+            allparts = _.compact( _.union([p.name], p.desc.split(' '), p.tags, [p.location]));
+            // common words
+            var common = _.intersection(word.synonyms, allparts);
+
+            if (common.length) {
+              // get the length of the matched words
+              pct = _.reduce(common, function(memo, val) {
+                return memo + val.length;
+              }, 0);
+
+              // add it to the total of matched words for the input
+              inputTotal += pct;
             }
-          }
+
+          });
+
+          // add the total to the collective count
+          count.push(inputTotal);
+        });
+
+        // console.log(count)
+
+
+        // now, find out which plugin won
+        var winner = _.max(count);
+        if (winner > 1) {
+          var winningPlugin = plugins[count.indexOf(winner)];
+          // console.log("contest winner had value of", winner, "matching chars", winningPlugin);
+
+          // print the winners
+          callback({
+            raw: raw,
+            words: data.words,
+            actions: data.actions,
+            winner: winningPlugin,
+            winningValue: winner
+          });
+        } else {
+          // not within threshhold
+          callback(null);
         }
-      ];
+      // }();
 
-      // try to match the query to a plugin
-      var count = [];
-      _.each(plugins, function(p) {
-        inputTotal = 0;
-        _.each(data.words, function(word) {
+    });
+  };
 
-          // find the common synonyms/tags between the plugin and the word
-          // all parts of the plugin (that matter)
-          allparts = _.compact( _.union([p.name], p.desc.split(' '), p.tags, [p.location]));
-          // common words
-          var common = _.intersection(word.synonyms, allparts);
+  this.matchMeaning = function(str, callback) {
 
-          if (common.length) {
-            // get the length of the matched words
-            pct = _.reduce(common, function(memo, val) {
-              return memo + val.length;
-            }, 0);
+    // start by breaking down the phrase into a more manageable form
+    this.breakDownPhrase(str, function(winned) {
 
-            // add it to the total of matched words for the input
-            inputTotal += pct;
-          }
-
-        });
-
-        // add the total to the collective count
-        count.push(inputTotal);
-      });
-
-      // console.log(count)
-
-
-      // now, find out which plugin won
-      var winner = _.max(count);
-      if (winner > 1) {
-        var winningPlugin = plugins[count.indexOf(winner)];
-        // console.log("contest winner had value of", winner, "matching chars", winningPlugin);
-
-        // print the winners
-        callback({
-          raw: raw,
-          words: data.words,
-          actions: data.actions,
-          winner: winningPlugin,
-          winningValue: winner
-        });
-      } else {
-        // not within threshhold
+      // no matches for the string
+      if (!winned) {
         callback(null);
+        return
       }
-    // }();
 
-  });
-};
+      // what type of request?
+      _.each(root.phraseTypes, function(explst, type) {
+        _.each(explst, function(exp) {
+          m = exp.exec(winned.raw);
+          if (m && m.length) {
 
-var getDataItemWithinInfo = function(str, callback) {
-  getCorrectRecord(str, function(winned) {
-    // console.log(typeof winned.words)
+            // get the operation done (look for the key name
+            // like 'enable')
+            operation = _.invert(root.phraseTypes)[
+              _.filter(root.phraseTypes, function(tv, tk) {
+                return tv === exp || _.contains(tv, exp);
+              })
+            ];
 
-    // no matches for the string
-    if (!winned) {
-      callback(null);
-      return
-    }
+            // ok, got the request type.
+            // what exactly are we testing?
 
-    // what type of request?
-    _.each(typeTests, function(explst, type) {
-      _.each(explst, function(exp) {
-        m = exp.exec(winned.raw);
-        if (m && m.length) {
+            // go through each data item, and
+            // get its value. Then, compare it to
+            // the main corpus
 
-          // get the operation done (look for the key name
-          // like 'enable')
-          operation = _.invert(typeTests)[
-            _.filter(typeTests, function(tv, tk) {
-              return tv === exp || _.contains(tv, exp);
-            })
-          ];
+            // create main corpus (stems of all parts of the original phrase)
+            maincorpus = root.stemArray(winned.raw.split(" "));
 
-          // ok, got the request type.
-          // what exactly are we testing?
+            // create place to store ranks
+            dataRanks = [];
 
-          // go through each data item, and
-          // get its value. Then, compare it to
-          // the main corpus
+            // get data items
+            _.each(winned.winner.data, function(v, kk) {
+              item = [v.label || kk];
 
-          // create main corpus (stems of all parts of the original phrase)
-          maincorpus = stemArray(winned.raw.split(" "));
+              // calculate rank for this data item
+              dataRanks.push({
+                name: kk,
+                value: _.intersection(
+                  root.stemArray(item),
+                  maincorpus
+                ).length
+              });
 
-          // create place to store ranks
-          dataRanks = [];
-
-          // get data items
-          _.each(winned.winner.data, function(v, kk) {
-            item = [v.label || kk];
-
-            // calculate rank for this data item
-            dataRanks.push({
-              name: kk,
-              value: _.intersection(
-                stemArray(item),
-                maincorpus
-              ).length
             });
 
-          });
+            // now, calculate the highest 'score'
+            winnerScore = _.max(
+              dataRanks.map(function(item) {
+                return item.value;
+              })
+            );
 
-          // now, calculate the highest 'score'
-          winnerScore = _.max(
-            dataRanks.map(function(item) {
-              return item.value;
-            })
-          );
+            // which name equates to that score?
+            dataWinnerName = dataRanks.filter(function(item) {
+              return item.value == winnerScore;
+            });
 
-          // which name equates to that score?
-          dataWinnerName = dataRanks.filter(function(item) {
-            return item.value == winnerScore;
-          });
+            // and, do some more formatting
+            if (dataWinnerName.length) {
 
-          // and, do some more formatting
-          if (dataWinnerName.length) {
-            dataWinner = dataWinnerName[0].name
-            callback(winned.winner, operation, dataWinner);
-          } else {
-            console.log("No clue which data item we're talking about")
+              // note: if there are mutiple winners with the same score, then
+              // the first will be chosen
+              dataWinner = dataWinnerName[0].name
+
+              // Finally, callback!
+              callback(winned.winner, operation, dataWinner);
+            } else {
+              callback(winned.winner, operation, null);
+            }
+
           }
-
-        }
+        });
       });
+
     });
 
-  });
 
+  };
+
+  // stem a whole array
+  this.stemArray = function(array) {
+    stems = [];
+    array.forEach(function(i) {
+      stems.push( natural.PorterStemmer.stem(i) );
+    });
+
+    return stems;
+  };
 
 };
 
-// perform the query
-var str = process.argv.slice(2).join(" ");
-var typeTests = {
-  equality: [/(.*) equals?(?: to)? (.*)/gi],
-  enable: [/(.*) on (?:the )?(.*)/gi, /enable/gi]
-}
-getDataItemWithinInfo(str, function(thing, operation, dataItem) {
+// test the query
+p = new Parser();
+p.matchMeaning(process.argv.slice(2).join(" "), function(thing, operation, dataItem) {
   if (thing) {
-    console.log(thing, operation, dataItem)
+    console.log(thing.name, operation, dataItem)
+  } else if (!thing) {
+    console.log("You need to be more discriptive (your description matched nothing)");
+  } else if (!dataItem) {
+    console.log("No clue which data item we're talking about within the thing");
   } else {
-    console.log("You need to be more discriptive (your description matched nothing)")
-  }
+    console.log("Something really weird happened -> No real error message.");
+  };
 })
